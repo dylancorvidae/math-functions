79c7622ac9cac9382902ce1fd98ceee52248a284
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-19 17:57:45 -0700

    Setting up stretch goal, not completed yet.

--- src/math-functions.js
+++ src/math-functions.js
@@ -121,25 +121,36 @@ export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
-
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
-
 // Don't forget to create a new branch for your work on the next question, if you attempt it.
-
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument 
+and returns an array whose first element is the product of those numbers, and the second element is a 
+string that EXACTLY follows this example and concatenates a message using the arguments that were passed 
+into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, 
+use your multiply() function that you've already created. You're going to have to be resourceful to 
+figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let arrayProduct = 1;
+    for(let arrayNums = 0;
+        arrayNums < dynamicArray.length;
+        arrayNums++){
+        arrayProduct = multiply(arrayProduct,);
+        console.log(arrayProduct);
+    }
 }
+
+//need to use array.length property somehow
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // You're done! Submit the link to the repo following the instructions in Canvas.
